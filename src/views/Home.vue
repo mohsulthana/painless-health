f5f3197179b659e7f20f95a15b9e380f974aecdf
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-08-13 07:07:29 +0700

    add new calendar resource view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <FullCalendar :options="calendarOptions" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin, resourceTimelinePlugin],
+        initialView: 'resourceTimeline',
+        headerToolbar: {
+          left: 'today prev,next',
+          center: 'title',
+          right: 'resourceTimelineDay,resourceTimelineWeek'
+        },
+        resources: [
+          { id: 'a', building: '460 Bryant', title: 'Auditorium A' },
+          { id: 'b', building: '460 Bryant', title: 'Auditorium B' },
+          { id: 'c', building: '460 Bryant', title: 'Auditorium C' },
+          { id: 'd', building: '460 Bryant', title: 'Auditorium D' },
+          { id: 'e', building: '460 Bryant', title: 'Auditorium E' },
+          { id: 'f', building: '460 Bryant', title: 'Auditorium F' },
+          { id: 'g', building: '564 Pacific', title: 'Auditorium G' },
+          { id: 'h', building: '564 Pacific', title: 'Auditorium H' },
+          { id: 'i', building: '564 Pacific', title: 'Auditorium I' },
+          { id: 'j', building: '564 Pacific', title: 'Auditorium J' },
+          { id: 'k', building: '564 Pacific', title: 'Auditorium K' },
+          { id: 'l', building: '564 Pacific', title: 'Auditorium L' },
+          { id: 'm', building: '564 Pacific', title: 'Auditorium M' },
+          { id: 'n', building: '564 Pacific', title: 'Auditorium N' },
+          { id: 'o', building: '101 Main St', title: 'Auditorium O' },
+          { id: 'p', building: '101 Main St', title: 'Auditorium P' },
+          { id: 'q', building: '101 Main St', title: 'Auditorium Q' },
+          { id: 'r', building: '101 Main St', title: 'Auditorium R' },
+          { id: 's', building: '101 Main St', title: 'Auditorium S' },
+          { id: 't', building: '101 Main St', title: 'Auditorium T' },
+          { id: 'u', building: '101 Main St', title: 'Auditorium U' },
+          { id: 'v', building: '101 Main St', title: 'Auditorium V' },
+          { id: 'w', building: '101 Main St', title: 'Auditorium W' },
+          { id: 'x', building: '101 Main St', title: 'Auditorium X' },
+          { id: 'y', building: '101 Main St', title: 'Auditorium Y' },
+          { id: 'z', building: '101 Main St', title: 'Auditorium Z' }
+        ]
+      }
+    }
+  },
   components: {
-    HelloWorld
+    FullCalendar
   }
 }
 </script>
