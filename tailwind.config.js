7ad3bcb8c7adaf5060c6c4395579782c78d9f3b6
sulthan <muhammadsultana@outlook.com>
2020-08-20 00:48:44 +0700

    customize fullCalendar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {}
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+      sans: ['-apple-system', 'BlinkMacSystemFont'],
+      serif: ['Georgia', 'Cambria'],
+      mono: ['SFMono-Regular', 'Menlo']
+    },
+    borderWidth: {
+      default: '1px',
+      0: '0',
+      2: '2px',
+      4: '4px'
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff'
+      },
+      spacing: {
+        96: '24rem',
+        128: '32rem'
+      }
+    }
   },
   variants: {},
   plugins: []
